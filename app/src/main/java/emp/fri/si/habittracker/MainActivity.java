2799a863c28d7dc2ec122ad57505f631d9f2248d
dejvv <zagorsek.david@gmail.com>
2017-12-26 17:28:29 +0100

    Initial commit

---
+++ app/src/main/java/emp/fri/si/habittracker/MainActivity.java
@@ -0,0 +1,13 @@
+package emp.fri.si.habittracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
